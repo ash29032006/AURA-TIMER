396c4099000d023d19edb4d1478822bf57830c65
Ashwin Harish <ashwinharish@MacBook-Air-97.local>
2026-04-19 14:38:44 +0530

    chore: remove development test scripts and clean up repository root

--- test-screenshot.cjs
+++
@@ -1,9 +0,0 @@
-const puppeteer = require('puppeteer');
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('http://localhost:5173', { waitUntil: 'networkidle0' });
-  await page.screenshot({ path: 'test-screenshot.png' });
-  console.log("Screenshot taken!");
-  await browser.close();
-})();
